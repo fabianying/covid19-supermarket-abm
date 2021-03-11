2a40e43964d6e336e67390817fafbacad59125e9
Fabian Ying <fabian.m.ying@gmail.com>
2021-03-11 09:07:06 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="covid19-supermarket-abm",
-    version="0.0.3",
+    version="0.0.4",
     author="Fabian Ying",
     author_email="fabian.m.ying@gmail.com",
     description='Agent-based model for Covid-19 transmission in supermarkets',
